66b83078eea34be90377087ed3679e42cad15200
Ammar <142759310+AmmarNabilF@users.noreply.github.com>
2025-09-23 02:31:57 +0800

    remove debug banner

--- todo_app_pemula/lib/main.dart
+++ todo_app_pemula/lib/main.dart
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Todo App Pemula by CRUX',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const TodoListScreen(),
